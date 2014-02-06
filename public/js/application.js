b7fd68cfac58d65654e00bc7b5b9c78185249639
Apprentice <apprentice@dbc-chicago-007.local>
2014-02-06 10:36:08 -0600

    box changes color at random

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+  $('a').click(function(){
+      // console.log(this); //i am on the button
+
+      //i want to select one of the boxes at random
+      //random number generator
+      var num = Math.floor((Math.random()*9)+1);
+
+      $($('ul').children()[num]).css('background-color', 'green'); //this is the random box
+
+
+
+
+
+  });
+
 });
